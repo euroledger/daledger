8b1eb6d6081d91463f00a917e56e6cd832d349ab
starlingseurope <mike.richardson@starlingsglobal.com>
2019-11-21 18:49:17 +0200

    fix unit tests

--- server/client/src/components/header/header.test.js
+++ server/client/src/components/header/header.test.js
@@ -1,27 +1,26 @@
-import React from "react";
-import { shallow } from "enzyme";
-import Header from "./index";
+import React from 'react';
+import { shallow } from 'enzyme';
+import LogoPanel from './LogoPanel';
 import { findByTestAttribute } from '../../utils';
 
 const setUp = (props = {}) => {
-  const component = shallow(<Header {...props}></Header>);
-  return component;
+    const component = shallow(<LogoPanel {...props}></LogoPanel>);
+    return component;
 };
 
+describe('Header Component', () => {
+    let component;
+    beforeEach(() => {
+        component = setUp();
+    });
 
-describe("Header Component", () => {
-  let component;
-  beforeEach(() => {
-    component = setUp();
-  });
+    it('should render withour errors', () => {
+        const wrapper = findByTestAttribute(component, 'headerComponent');
+        expect(wrapper.length).toBe(1);
+    });
 
-  it("should render withour errors", () => {
-    const wrapper = findByTestAttribute(component, 'headerComponent');
-    expect(wrapper.length).toBe(1);
-  });
-
-  it("should render a logo", () => {
-    const logo = findByTestAttribute(component, 'logoIMG');
-    expect(logo.length).toBe(1);
-  });
+    it('should render a logo', () => {
+        const logo = findByTestAttribute(component, 'logoIMG');
+        expect(logo.length).toBe(1);
+    });
 });
