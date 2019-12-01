888a6898ca575519ec5f91d2445d0634c520aad9
starlingseurope <mike.richardson@starlingsglobal.com>
2019-12-01 20:16:28 +0200

    back out all tests

--- server/client/src/components/header/headertest.js
+++ server/client/src/components/header/headertest.js
@@ -2,23 +2,16 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import LogoPanel from './LogoPanel';
 import { findByTestAttribute } from '../../utils';
-import HeaderForTests from '.';
-import ButtonAppBar from './ButtonAppBar';
 
 const setUp = (props = {}) => {
     const component = shallow(<LogoPanel {...props}></LogoPanel>);
     return component;
 };
-const setUpHdr = (props = {}) => {
-    const component = shallow(<HeaderForTests {...props}></HeaderForTests>);
-    return component;
-};
 
 describe('Header Component', () => {
-    let component, component2;
+    let component;
     beforeEach(() => {
         component = setUp();
-        component2 = setUpHdr();
     });
 
     it('should render withour errors', () => {
--- server/index.js
+++ server/index.js
@@ -30,6 +30,8 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
+
+
 log4js.configure({
     appenders: {
         file: {
