6ac0ae6a7678e8c181780f74328cadf47112856c
starlingseurope <mike.richardson@starlingsglobal.com>
2019-12-05 16:36:30 +0200

    Alter Logo position for mobile apps

--- server/client/src/components/header/LogoPanel.js
+++ server/client/src/components/header/LogoPanel.js
@@ -6,14 +6,16 @@ import './styles.scss';
 
 const useStyles = makeStyles(theme => ({
     header: {
-        paddingLeft: theme.spacing(0),
+        // paddingLeft: theme.spacing(0),
+        
         height: '10em',
         [theme.breakpoints.down('md')]: {
             height: '4.5em'
         },
-        textAlign: 'center'
     },
+  
     img: {
+        margin: '0 auto',
         maxWidth: '20em',
         [theme.breakpoints.down('md')]: {
             maxWidth: '15em',
@@ -29,7 +31,7 @@ const LogoPanel = ({language}) => {
     const classes = useStyles();
     return (
         <header data-test='logopanelComponent' className={classes.header}>
-            <div>
+            <div >
                <LogoImage language={language}/>
             </div>
         </header>
