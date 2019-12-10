831c74678fc9f78d86dd79628aa33d8a2d822b7b
starlingseurope <mike.richardson@starlingsglobal.com>
2019-12-10 17:33:22 +0200

    Google Authentication; fix build

--- server/client/src/components/header/MobileButtonAppBar.js
+++ server/client/src/components/header/MobileButtonAppBar.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
