8f93f73bedb117b7b7c26f3cc02bb2b6d219ebb3
starlingseurope <mike.richardson@starlingsglobal.com>
2019-12-10 10:23:41 +0200

    direct logging to stdout for test & prod

--- server/config/index.js
+++ server/config/index.js
@@ -2,27 +2,32 @@ const dotenv = require('dotenv');
 const log4js = require('log4js');
 var logger = log4js.getLogger('config.js');
 
-log4js.configure({
-    appenders: {
-        file: {
-            type: 'file',
-            filename: 'logs/server.log',
-            maxLogSize: 10485760,
-            numBackups: 3
-        }
-    },
-    categories: { default: { appenders: ['file'], level: 'debug' } }
-});
-
-logger.level = 'all';
-dotenv.config();
-console.log('STARTING...');
-
 var environment = process.env.NODE_ENV || 'development';
 if (process.env.TEST_ENV === 'test') {
     environment = 'test';
 }
-console.log('QUACK environment = ', environment);
+if (environment === 'development') {
+    log4js.configure({
+        appenders: {
+            file: {
+                type: 'file',
+                filename: 'logs/server.log',
+                maxLogSize: 10485760,
+                numBackups: 3
+            }
+        },
+        categories: { default: { appenders: ['file'], level: 'debug' } }
+    });
+} else {
+    log4js.configure({
+        appenders: { out: { type: 'stdout' } },
+        categories: { default: { appenders: ['out'], level: 'debug' } }
+    });
+}
+
+logger.level = 'all';
+dotenv.config();
+logger.info('STARTING...');
 
 let SERVER_ENV;
 if (environment === 'development') {
