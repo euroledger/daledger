14bec87760ff8e2ba974a6f092339186737983f7
starlingseurope <mike.richardson@starlingsglobal.com>
2019-12-10 09:33:56 +0200

    debug

--- server/config/index.js
+++ server/config/index.js
@@ -6,6 +6,7 @@ var environment = process.env.NODE_ENV || 'development';
 if (process.env.TEST_ENV==='test') {
     environment = 'test';
 }
+console.log("QUACK environment = ", environment);
 
 var serverConf = SERVER_ENV[environment];
 module.exports = { serverConf, environment };
