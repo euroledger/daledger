f650d5458eca734ffdb2a925a61e1531726f6064
starlingseurope <mike.richardson@starlingsglobal.com>
2019-12-10 09:50:20 +0200

    more debugging

--- server/config/index.js
+++ server/config/index.js
@@ -1,13 +1,15 @@
 const SERVER_ENV = require('./config');
 const dotenv = require('dotenv');
 var logger = log4js.getLogger('config.js');
+console.error("STARTING...");
+console.error("TEST_ENV = ", process.env.TEST_ENV);
 dotenv.config();
 
 var environment = process.env.NODE_ENV || 'development';
 if (process.env.TEST_ENV==='test') {
     environment = 'test';
 }
-logger.info("QUACK environment = ", environment);
+console.error("QUACK environment = ", environment);
 
 var serverConf = SERVER_ENV[environment];
 module.exports = { serverConf, environment };
