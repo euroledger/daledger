87f9a1ee557a842e8f7a201bce38fb40bd7a4eee
starlingseurope <mike.richardson@starlingsglobal.com>
2019-12-10 09:41:51 +0200

    add logging to config.js

--- server/config/index.js
+++ server/config/index.js
@@ -1,12 +1,13 @@
 const SERVER_ENV = require('./config');
 const dotenv = require('dotenv');
+var logger = log4js.getLogger('config.js');
 dotenv.config();
 
 var environment = process.env.NODE_ENV || 'development';
 if (process.env.TEST_ENV==='test') {
     environment = 'test';
 }
-console.log("QUACK environment = ", environment);
+logger.info("QUACK environment = ", environment);
 
 var serverConf = SERVER_ENV[environment];
 module.exports = { serverConf, environment };
