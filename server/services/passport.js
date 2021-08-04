1bfebdcb9e33367dd2913b6c12b016bcd2a17380
mikeproofmarket <mrichardson@proofmarket.io>
2021-08-04 12:20:12 +0300

    test database stuff

--- server/services/passport.js
+++ server/services/passport.js
@@ -29,20 +29,21 @@ passport.use(
         },
         async (accessToken, refreshToken, profile, done) => {
         
-            logger.info('We are in callback');
-            const existingUser = await User.findOne({ googleId: profile.id }); // query db to see if user already exists
-            logger.info('Got user');
-            if (existingUser) {
-                // TODO retrieve fabric keys from wallet here...if they don't exist reject this login...
+            logger.info('We are in callback, id = ', profile.id);
+            // const existingUser = await User.findOne({ googleId: profile.id }); // query db to see if user already exists
+            // logger.info('Got user');
+            // if (existingUser) {
+            //     // TODO retrieve fabric keys from wallet here...if they don't exist reject this login...
 
-                logger.info('Google authentication ok, id = ', profile.id);
-                return done(null, existingUser);
-            }
+            //     logger.info('Google authentication ok, id = ', profile.id);
+            //     return done(null, existingUser);
+            // }
             // if we have registration info (in state store?) then call Fabric to enroll and register
             // new keys...store in wallet and then save info in MongoDB
             // otherwise reject "you must register ...etc"
             const user = await new User({ googleId: profile.id }).save();
 
+            logger.info("DONE THE DB STUFF");
             done(null, user);
         }
     )
